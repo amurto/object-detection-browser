813c99e9554cbe5930d6be803cbc7bffe446ddc5
amurto <amurtobasu@gmail.com>
2020-04-19 16:00:14 +0530

    docs

---
+++ docs/precache-manifest.f7877cf72253a5e27075a2812db17fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5017fbcc00c809fb0c7298992f0e700",
+    "url": "/object-detection-browser/index.html"
+  },
+  {
+    "revision": "b9ef4d729207879f1977",
+    "url": "/object-detection-browser/static/css/main.8ab6ef03.chunk.css"
+  },
+  {
+    "revision": "47378f5194f800f1c387",
+    "url": "/object-detection-browser/static/js/2.22cb919d.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/object-detection-browser/static/js/2.22cb919d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ef4d729207879f1977",
+    "url": "/object-detection-browser/static/js/main.a301e1f3.chunk.js"
+  },
+  {
+    "revision": "c762a4dcd056966183e2",
+    "url": "/object-detection-browser/static/js/runtime-main.c09ec851.js"
+  }
+]);
